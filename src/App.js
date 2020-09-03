410dee5fad8b17d0ed7cb38a6cbd235a87de2b3a
unknown <welton.c.lima@gmail.com>
2020-09-03 02:40:04 -0300

    JSHunter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Routes from './routes';
+import './styles.css';
+import Main from './pages/main';
+import Header from "./components/Header";
 
-function App() {
-  return (
+const App = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header />
+    <Routes />
     </div>
   );
-}
 
 export default App;
